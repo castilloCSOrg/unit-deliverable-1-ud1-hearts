a3d801b0dfa9377bfb1abf4d3ed5d643885445c8
anthonyannis <aannis@miracosta.edu>
2024-10-09 17:03:45 +0000

    v1 so far 10:04 am on 10/9/24

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
 /********************************************
-*	AUTHORS:	<your names>
-* COLLABORATORS: <name of peer, tutor, instructor, anyone else who helped>
-*	LAST MODIFIED:	<date of last change>
+*	AUTHORS:	<Anthony Annis>
+* COLLABORATORS: <myself>
+*	LAST MODIFIED:	<>
 ********************************************/
 
 /********************************************
-*	<TITLE OF PROGRAM>
+*	<Weight Loss Calculator>
 *********************************************
 *	PROGRAM DESCRIPTION:
 *	<1-2 sentences describing overall program>
@@ -16,24 +16,48 @@
 *	STATIC METHODS:
 * <list of static methods and which teammate made each>
 *********************************************/
-
+import java.util.Scanner;
 
 public class Main 
 {
-  /***** CONSTANT SECTION *****/
+ public static void main(String[] args)
+ {
+   try (Scanner scanner = new Scanner(System.in))
+   {
+    System.out.print("Enter your body weight(in pounds): ");
+    double bodyWeight = scanner.nextDouble();
 
-  public static void main(String[] args)
+    System.out.print("Enter pounds to lose: ");
+    double pounds = scanner.nextDouble();
+
+    System.out.print("Enter weeks to achieve this goal: ");
+    int weeks = scanner.nextInt();
+
+    double dailyCalorieDeficit = calculateCalorieDeficit(bodyWeight, pounds, weeks);
+
+    double maintenanceCalories = bodyWeight * 15;
+    double totalCalorieDeficit = (pounds * 15) - (bodyWeight * 10.5);
+    double gramsOfProtein = bodyWeight * 0.82; 
+
+    formatOutput(pounds, weeks, maintenanceCalories, totalCalorieDeficit, dailyCalorieDeficit, gramsOfProtein);
+
+    scanner.close();
+
+  }
+
+  }
+
+  public static double calculateCalorieDeficit(double bodyWeight, double pounds, int weeks)
   {
-    /***** DECLARATION SECTION *****/
-
-    /***** INITIALIZATION SECTION *****/
-
-    /***** INTRO SECTION *****/
-
-    /***** PROCESSING SECTION *****/
-
-    /***** OUTPUT SECTION *****/
+    double totalCalorieDeficit = (maintenanceCalories) - (pounds * 10.5);
+    double dailyCalorieDeficit = totalCalorieDeficit / weeks;
     
   }
-  /***** STATIC METHODS *****/
+
+  public static void formatOutput(double pounds, int weeks, double maintenanceCalories, double deficitAmount, double dailyCalorieDeficit)
+  {
+  System.out.printf("To lose %.1f pounds in %d weeks, you need to cut %.0f calories per day.%n", pounds, weeks, dailyCalorieDeficit);
+  System.out.printf("Maintenance Calories: %.0f calories %n", maintenanceCalories);
+  System.out.printf("Deficit Amount: %.0f calories%n", deficitAmount);      
+  }
 }
